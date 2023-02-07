77b132ae79898ffd5a7d8f6be134f4300277cb8c
Fatis5 <fheredia1121@conalepmex.edu.mx>
2023-02-07 13:51:21 -0600

    ook

--- videoconfianza/src/littleComponents/CartButton.jsx
+++ videoconfianza/src/littleComponents/CartButton.jsx
@@ -4,25 +4,21 @@ import { useContext } from "react";
 import { ContextCredentials } from "../ContextCredentials";
 
 
-const CartButton = ({
-  id,
-  modelo,
-  precio,
-  imagen,
-  cantidad,
-  titulo,
-  marca,
-}) => {
+const CartButton = ({ id, modelo, precio, img, cantidad, titulo, marca }) => {
   const { Cart, setCart } = useContext(ContextCredentials);
-  const [Cantidad, setCantidad] = useState(0);
+  const [Cantidad, setCantidad] = useState(
+    Cart.find((item) => item.id === id)
+      ? Cart.find((item) => item.id === id).cantidad
+      : 0
+  );
 
   const AddCart = (id, modelo, precio, imagen, cantidad, titulo, marca) => {
     const producto = {
       id,
       modelo,
       precio,
-      imagen,
-      cantidad,
+      img,
+      cantidad: Cantidad + 1,
       titulo,
       marca,
     };
@@ -34,34 +30,84 @@ const CartButton = ({
 
       if (existe) {
         const newCart = Cart.map((item) =>
-          item.id === id ? { ...existe, cantidad: Cantidad } : item
+          item.id === id ? { ...existe, cantidad: Cantidad + 1 } : item
         );
         setCart(newCart);
       } else {
         setCart([...Cart, producto]);
       }
     }
+    console.log(Cart);
   };
-  
+
+ //function to remove products from cart when press - button with lodash
+  const RemoveCart = (id, modelo, precio, img, cantidad, titulo, marca) => {
+    const producto = {
+      id,
+      modelo,
+      precio,
+      img,
+      cantidad: Cantidad - 1,
+      titulo,
+      marca,
+    };
+
+    if (Cart.length === 0) {
+      setCart([...Cart, producto]);
+    } else {
+      const existe = Cart.find((item) => item.id === id);
+
+      if (existe) {
+        const newCart = Cart.map((item) =>
+          item.id === id ? { ...existe, cantidad: Cantidad - 1 } : item
+        );
+        setCart(newCart);
+      } else {
+        setCart([...Cart, producto]);
+      }
+    }
+    console.log(Cart);
+  };
+
+
 
   return (
-    <div className="flex flex-row mt-5 justify-center w-full mb-3">
-      <input
-        onChange={(e) => setCantidad(e.target.value)}
-        className="w-1/3 h-11/12 mr-5 border-2 border-gray-300 bg-white h-10 px-5  rounded-lg text-sm focus:outline-none"
-        type="number"
-        value={Cantidad}
-      />
+    <div className="flex flex-row mt-5 justify-center w-full mb-3 px-3">
+      <div className=" px-5 w-auto bg-red-500 align-middle h-11/12 rounded-md flex items-center justify-center">
+        <button
+        disabled={Cantidad === 0 ? true : false}
+          onClick={() => {
+            setCantidad(Cantidad - 1);
+            RemoveCart(id, modelo, precio, img, cantidad, titulo, marca);
+          }}
+          className="flex flex-row "
+        >
+          {/* <AiOutlineShoppingCart className="text-white my-auto text-xl" /> */}
+          <p className="text-white text-3xl">-</p>
+        </button>
+      </div>
+      <p
+        className="
+        w-10 h-11/12 bg-white text-center text-xl font-bold
+
+      
+      "
+      >
+        {Cart.find((item) => item.id === id)
+          ? Cart.find((item) => item.id === id).cantidad
+          : 0}
+      </p>
 
       <div className=" px-5 w-auto bg-yellow-400 align-middle h-11/12 rounded-md flex items-center justify-center">
         <button
           onClick={() => {
-            AddCart(id, modelo, precio, imagen, cantidad, titulo, marca);
+            setCantidad(Cantidad + 1);
+            AddCart(id, modelo, precio, img, cantidad, titulo, marca);
           }}
           className="flex flex-row "
         >
-          <AiOutlineShoppingCart className="text-white my-auto text-xl" />
-          <p className="text-white">Agregar</p>
+          {/*   <AiOutlineShoppingCart className="text-white my-auto text-xl" /> */}
+          <p className="text-white text-3xl">+</p>
         </button>
       </div>
     </div>
